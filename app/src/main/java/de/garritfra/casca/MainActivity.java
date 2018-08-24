03b2ea0c14a4737ae208468eaaf2c700795205e8
garritfra <garritfranke@gmail.com>
2018-08-24 21:31:33 +0200

    add inventory activity

--- app/src/main/java/de/garritfra/casca/MainActivity.java
+++ app/src/main/java/de/garritfra/casca/MainActivity.java
@@ -1,7 +1,12 @@
 package de.garritfra.casca;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import de.garritfra.casca.view.ItemListActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button viewInventoryButton = findViewById(R.id.view_inventory_button);
+
+        viewInventoryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent viewInventoryIntent = new Intent(MainActivity.this, ItemListActivity.class);
+                MainActivity.this.startActivity(viewInventoryIntent);
+            }
+        });
     }
 }
